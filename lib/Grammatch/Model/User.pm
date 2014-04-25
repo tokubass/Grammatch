f54ed9119ff1a008a6f1dabf4d9eceb0b87c723c
papix <mail@papix.net>
2014-04-26 00:06:47 +0900

    Implement dojo list for user

--- lib/Grammatch/Model/User.pm
+++ lib/Grammatch/Model/User.pm
@@ -9,5 +9,14 @@ sub user {
     return c->db->single('user' => { user_id => $user_id }) || undef;
 }
 
+sub dojo_list {
+    my ($class, $user_id) = @_;
+    return undef unless $user_id; 
+    return c->db->search_by_sql(
+        'SELECT * FROM user_dojo_map JOIN dojo ON user_dojo_map.dojo_id = dojo.dojo_id WHERE user_dojo_map.user_id = ? AND status = 1',
+        [ $user_id ],
+    );
+}
+
 1;
 
--- lib/Grammatch/Web/C/User.pm
+++ lib/Grammatch/Web/C/User.pm
@@ -9,10 +9,13 @@ sub user {
 
     my $user_data = Grammatch::Model::User->user($user_id);
     return $c->redirect('/') unless $user_data; 
+    my $dojo_list = Grammatch::Model::User->dojo_list($user_id);
+    return $c->redirect('/') unless $dojo_list; 
     
     return $c->render('user/user.tx',{
         user_data => $user_data,
         dojo_data => $user_data->dojo,
+        dojo_list => $dojo_list,
     });
 }
 
